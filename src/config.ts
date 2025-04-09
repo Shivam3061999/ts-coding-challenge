13743f1012022f2c8dad610d978682b9f03efaf9
Shivam_Kumar <shivamkumar3061999@gmail.com>
2025-04-10 02:58:09 +0530

    commit all

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,10 @@ export interface Account {
 }
 
 export const accounts: Account[] = [
-  {id: "0.0.5838509", privateKey: "a632f446d2d08b4f2f7f7ae2016ca62c0da4a0dd1bc07f9b8ad3f0b1dbd63115"},
+  {id: "0.0.5838476", privateKey: "0433d1cf9beb8c495309bf481b95f7392231f3caf03919a92e312ae2bd4fa101"},
   {id: "0.0.5838501", privateKey: "4f0bd225fd01815bbd0ff8429d6f2e82f376ead114839d92df4cdd133c696ff3"},
   {id: "0.0.5838460", privateKey: "bb7fd45b99066bb29d7844955e1424737f53fe71eb0295a87140ec0f167c6b30"},
-  {id: "0.0.5838476", privateKey: "0433d1cf9beb8c495309bf481b95f7392231f3caf03919a92e312ae2bd4fa101"},
+  {id: "0.0.5838509", privateKey: "a632f446d2d08b4f2f7f7ae2016ca62c0da4a0dd1bc07f9b8ad3f0b1dbd63115"},
   {id: "0.0.5840472", privateKey: "eac2f83738a38c30bbd97027ecbdc3092b65064e3b34558d1852be0e47fe397c"}
   
 ]
